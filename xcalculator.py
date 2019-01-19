14dec586f63d5c63ea062884595afcf60432edf9
tchinyamakobvu <tchinyamakobvu@gmail.com>
2019-01-19 16:18:29 -0500

    calculator fucntions performing mathematical operations

---
+++ xcalculator.py
@@ -0,0 +1,29 @@
+def multiply(a,b):
+    return a * b
+    
+def subtract(a,b):
+	return (a-b)
+
+def add(a,b):
+	return (a + b)
+
+def divide(a,b):
+	return(a/b)
+
+
+
+print("I'm going use the calculator functions to multiply 5 and 6")
+x = multiply(5,6)
+print(30)
+
+print("I'm going use the calculator functions to subtract 5 and 6")
+x = subtract(5,6)
+print(-1)
+
+print("I'm going use the calculator functions to add 5 and 6")
+x = add(5,6)
+print(11)
+
+print("I'm going use the calculator functions to devide 10 and 2")
+x = divide(10,2)
+print(5)
